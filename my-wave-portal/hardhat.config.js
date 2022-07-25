035fe4f2dbfacc6eafe282b3b76ccd68ef6c66e3
zhangxiangyu <xiangyu.zhang@yo-star.com>
2022-07-25 18:28:01 +0800

    refactor: 检查是否连接钱包陈宫

--- my-wave-portal/hardhat.config.js
+++ my-wave-portal/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby:{
+      url:"https://eth-rinkeby.alchemyapi.io/v2/a0ZA6TtUZ5z9qCWJ6ohNAdiTwA6iEdOi",
+      accounts: ["a11247a03a4be4808433fbc82b4bd262735a701e8346eae379b25791f0b13156"]
+    }
+  }
 };
